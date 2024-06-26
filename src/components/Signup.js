697407da3e9fb00062090ceb149b855cb44b8706
mahmood-ulhassan <mahmood.hassan7114@gmail.com>
2024-06-26 01:58:17 -0700

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,7 +7,7 @@ function Signup({ showAlert }) {
 
   const signup = (e) => {
     e.preventDefault();
-    fetch("http://localhost:5000/api/auth/signup", {
+    fetch("https://i-notebook-api-three-iota.vercel.app/api/auth/signup", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
