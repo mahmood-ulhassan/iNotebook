627b5ddb94f4fed6ab77deedf44af7f9770ec6f8
mahmood-ulhassan <mahmood.hassan7114@gmail.com>
2024-06-26 06:17:02 -0700

    Cors update

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -11,6 +11,7 @@ function Signup({ showAlert }) {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
+    "mode":"no-cors"
   },
   body: JSON.stringify({
     name: credentials.name,
